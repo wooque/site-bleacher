74e5658e1fb2c139ce0371f46a3009829e20859c
Vuk Mirovic <wooque@gmail.com>
2018-12-29 21:52:17 +0100

    Firefox compatibility

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm site-bleacher.zip
-zip -r site-bleacher.zip . -x ".*" -x README.md -x "node_modules*" -x "package*.json"
+zip -r site-bleacher.zip . -x ".*" -x README.md -x "node_modules*" -x "package*.json" -x "package.sh"
